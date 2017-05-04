b6113fa798e3a069ef9f2f23d898a5436c25f10f
Darío Martín <dariomartinlara@gmail.com>
2017-05-05 01:52:29 +0200

    Videos list + design + launching youtube

--- app/src/main/java/com/fasttrack/android/movies/VideosModule.java
+++ app/src/main/java/com/fasttrack/android/movies/VideosModule.java
@@ -1,13 +1,14 @@
 package com.fasttrack.android.movies;
 
 import android.content.Context;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.content.Intent;
+import android.net.Uri;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.fasttrack.android.movies.adapters.VideosAdapter;
 import com.fasttrack.android.movies.models.MovieVideos;
@@ -20,7 +21,30 @@ import java.util.List;
 
 public class VideosModule extends LinearLayout {
 
-    private ListView videoList;
+    private final static String YOUTUBE_VIDEO_URL = "http://www.youtube.com/watch?v=";
+    private static final String YOUTUBE = "YouTube";
+
+    private LinearLayout videoList;
+    private OnClickListener onVideoClickListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int position = (int) v.getTag();
+            if (!videos.isEmpty() && videos.size() > position) {
+                MovieVideos.TMDBVideo video = videos.get(position);
+                String baseUrl = null;
+                switch (video.getSite()) {
+                    case YOUTUBE:
+                        baseUrl = YOUTUBE_VIDEO_URL;
+                        break;
+                }
+                if (baseUrl != null) {
+                    getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(baseUrl + videos.get(position).getKey())));
+                }
+            }
+        }
+    };
+
+    private List<MovieVideos.TMDBVideo> videos;
 
     public VideosModule(Context context) {
         super(context);
@@ -34,12 +58,22 @@ public class VideosModule extends LinearLayout {
 
     private void init(Context context) {
         inflate(context, R.layout.videos_module, this);
-        videoList = (ListView) findViewById(R.id.video_list_view);
+        videoList = (LinearLayout) findViewById(R.id.video_linear_layout);
     }
 
     public void addVideos(List<MovieVideos.TMDBVideo> videos) {
-        VideosAdapter adapter = new VideosAdapter(getContext(), videos);
-        videoList.setAdapter(adapter);
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+
+        this.videos = videos;
+
+        for (int i = 0; i < videos.size(); i++) {
+            View item = inflater.inflate(R.layout.video_item, null, false);
+            TextView videoName = (TextView) item.findViewById(R.id.video_item_name);
+            videoName.setText(videos.get(i).getName());
+            item.setTag(i);
+            item.setOnClickListener(onVideoClickListener);
+            videoList.addView(item);
+        }
     }
 
 }
